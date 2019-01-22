6428e31d1b3618e3a4c493078bfefce80ff64521
simone.rainieri <simone.rainieri@gmail.com>
2019-01-22 13:32:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 console.log(chocolateBars);
 
 function addElementToBeginningOfArray() {
-  var 1 = ["a", "b", "c"]
-  
+  var test = ["a", "b", "c"]
+  test.push("d")
+  return 
 }
